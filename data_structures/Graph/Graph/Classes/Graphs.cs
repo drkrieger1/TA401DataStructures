62c39b554329ba5aeefd5f0b0f71d2d8dd7305b5
drkrieger1 <eplyushko@gmail.com>
2018-07-09 20:36:43 -0700

    graph methods implemented

--- data_structures/Graph/Graph/Classes/Graphs.cs
+++ data_structures/Graph/Graph/Classes/Graphs.cs
@@ -26,24 +26,63 @@ namespace Graph.Classes
         //<Summery>
         //
         //</Summery>
-        public List<Node> GetNodes(Node root)
+        public List<Node> GetNodes()
         {
             List<Node> order = new List<Node>();
             Queue<Node> breadth = new Queue<Node>();
 
-            breadth.Enqueue(root);
-            root.Visited = true;
-            while(breadth.TryPeek(out root))
+            breadth.Enqueue(Root);
+            Root.Visited = true;
+            while(breadth.TryPeek(out Node result))
             {
+                Node front = breadth.Dequeue();
+                order.Add(front);
 
+                foreach(Node child in front.Children)
+                {
+                    if (!child.Visited)
+                    {
+                        child.Visited = true;
+                        breadth.Enqueue(child);
+                    }
+                }
             }
             return order;       
         }
 
         //GetNeighbors()
+        public List<Node> GetNeeighbors(Node root)
+        {
+            return root.Children;
+        }
 
         //Size()
-
+        public int Size(Node root)
+        {
+            List<Node> Total = BreadthFirst(root);
+            return Total.Count;
+        }
         //BreadthFirst()
+        public List<Node> BreadthFirst(Node root)
+        {
+            Queue<Node> breadth = new Queue<Node>();
+            List<Node> order = new List<Node>();
+            breadth.Enqueue(root);
+            root.Visited = true;
+
+            while (breadth.TryPeek(out root))
+            {
+                Node first = breadth.Dequeue();
+                order.Add(first);
+                foreach(Node child in first.Children)
+                {
+                    if (!child.Visited)
+                    {
+                        breadth.Enqueue(child);
+                    }
+                }
+            }
+            return order;
+        }
     }
 }
