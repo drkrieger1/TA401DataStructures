f9cf9ed20f53e9cd29c7a1e3b7471b1bc5c545ac
drkrieger1 <eplyushko@gmail.com>
2018-07-25 15:31:24 -0700

    node class created

---
+++ data_structures/LinkList/LinkList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkList
+{
+    public class Node
+    {
+        //Data property
+        public int Value { get; set; }
+        //Pointer to the next node
+        public Node Next { get; set; }
+
+        //Constructore for Node class
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
