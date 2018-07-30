10a659ddd0a385fb112683f2d00ffff20135a545
drkrieger1 <eplyushko@gmail.com>
2018-07-29 21:29:11 -0700

    Binary Search

---
+++ data_structures/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BinarySearch");
+
+            BinarySearcher search = new BinarySearcher();
+
+            int[] test = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int find = -1 ;
+            int result = search.BinarySearch(test, find);
+
+            if(result == -1)
+            {
+                Console.WriteLine($"{find} is not in the array");
+            }
+            else
+            {
+                Console.WriteLine($"index of:{find} is {result}");
+            }
+            Console.Read();
+        }
+    }
+}
