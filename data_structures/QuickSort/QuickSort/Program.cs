a54c3b6160dbd720611eec840c3911848f605d14
drkrieger1 <eplyushko@gmail.com>
2018-07-31 16:20:22 -0700

    Quick sort complete

--- data_structures/QuickSort/QuickSort/Program.cs
+++ data_structures/QuickSort/QuickSort/Program.cs
@@ -9,53 +9,76 @@ namespace QuickSort
             Console.WriteLine("Quick Sort");
 
             int[] test = { 51, 95, 66, 72, 42, 38, 41, 15 };
-            QuickSort(test);
+
+            int[] test2 = { 15, 3, 2, 35, 40, 25 };
+            Console.WriteLine("Array In: ");
+            Print(test);
+            Console.WriteLine();
+
+            int [] result = QuickSort(test, 0, test.Length - 1);
+            Console.WriteLine("Array Sorted: ");
+            Print(result);
+
 
             Console.Read();
         }
-
-        public static int[] QuickSort(int[] arr)
-        {
-            int pivot = 0;
-            int front = 1;
-            int end = arr.Length - 1;
-
-            while(front < end)
+        //quickSort method
+        //Input: unsorted array, Left index, Right index
+        public static int[] QuickSort(int[] arr, int left, int right)
+        {   
+            //Base case: breaks recursion if the left index
+            //passes or reaches the right index
+            if(left >= right)
             {
-                //check front against the pivot value
-                if(arr[pivot] < arr[front])
+                return arr;
+            }
+            
+            //Picking a middle pivot
+            int pivot = arr[(left + right) / 2];
+
+            //partision
+            int index = Partision(arr, left, right, pivot);
+            //call partision on the left and right
+            QuickSort(arr, left, index - 1);
+            QuickSort(arr, index, right);
+            return arr;
+        }
+
+        public static int Partision(int[] arr, int left, int right, int pivot)
+        {   
+            //Loop till left reaches right
+            while(left <= right)
+            {   
+                //find the number that on the left that is less than the pivot
+                while(arr[left] < pivot)
                 {
-                    int temp = arr[end];
-                    arr[end] = arr[front];
-                    arr[front] = temp;
-                    front++;
-                    end--;
+                    left++;
                 }
-                //check Rear against the pivot value
+                //find the number that is greater than the pivot
+                while(arr[right] > pivot)
+                {
+                    right--;
+                }
+                //Swap left and right
+                if(left <= right)
+                {
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                    left++;
+                    right--;
+                }
             }
-
-            if(arr[end] < arr[pivot])
-            {
-                int temp = arr[pivot];
-                arr[pivot] = arr[end];
-                arr[end] = temp;
-                pivot = end;
-            }
-            //sort left 
-            arr = SubSort(arr, pivot, pivot - 1);
-
-            //Sort Right
-            arr = SubSort(arr, pivot, arr.Length - 1);
-
-            Console.Read();
-
-            return arr;
+            //return left the, it splits the array. 
+            return left;
         }
 
-        public static int[] SubSort(int[] arr, int pivot, int end)
+        public static void  Print(int[] arr)
         {
-
-            return arr;
+            foreach(int n in arr)
+            {
+                Console.Write($"{n} ");
+            }
         }
     }
 }
